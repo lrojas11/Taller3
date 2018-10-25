d73ba29728e3d0e4e1c5f3254612fe3656c7a181
Laura Rojas <42391116+lrojas11@users.noreply.github.com>
2018-10-25 10:46:50 -0500

    Add files via upload
    Fourier parte 1 (Grafica de los datos de Signal.dat)

--- Fourier.py
+++ Fourier.py
@@ -1,3 +1,25 @@
 #Fourier.py
 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Almacenar datos
+
+signal = np.loadtxt('signal.dat', delimiter = ',')
+
+incompletos = np.loadtxt('incompletos.dat', delimiter = ',')
+
+#Grafica de signal.dat
+
+
+c1 = signal[:,0]
+c2 = signal[:,1]
+
+plt.title('Signal')
+plt.grid()
+plt.plot(c1,c2, color = 'crimson')
+plt.savefig('RojasLaura_signal.pdf')
+
+#Transformada discreta de Fourier a los datos de signal
+
+#def transformada ():
