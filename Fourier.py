402757c5cc00b0b591037ff5c30d909b711b0b52
Laura Rojas <42391116+lrojas11@users.noreply.github.com>
2018-10-29 11:38:36 -0500

    Add files via upload
    Fourier

--- Fourier.py
+++ Fourier.py
@@ -1,6 +1,7 @@
 #Fourier.py
 
 import numpy as np
+from scipy.fftpack import fftfreq
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
@@ -17,19 +18,41 @@ cs1 = signal[:,0]
 cs2 = signal[:,1]
 
 plt.title('Signal')
+plt.xlabel('Tiempo')
+plt.ylabel('Amplitud')
 plt.grid()
 plt.plot(cs1,cs2, color = 'crimson')
 plt.savefig('RojasLaura_signal.pdf')
 
 #Transformada discreta de Fourier a los datos de signal
 
-#def transformada (cs1,cs2):
 
+def transformada (cs1,cs2):
+    N = len(cs2)
+    lista_real = []
+    lista_imaginaria = []
+    for k in range(N):
+        suma_real= 0
+        suma_imaginaria = 0
+        for n in range (N):
+            suma_real = suma_real + np.cos((-2*np.pi*k*n)/N) * cs2[n]
+            suma_imaginaria = suma_real + np.sin((-2*np.pi*k*n)/N) * cs2[n]
+        lista_real.append (suma_real)
+        lista_imaginaria.append (suma_imaginaria)
+    return lista_real,lista_imaginaria
+lista_real, lista_imaginaria = transformada (cs1,cs2)
 
 
 
 #Grafica de transformada de Fourier
 
+periodo = cs1[1]-cs1[0]
+N = len(cs1)
+frecuencias = fftfreq(N,periodo)
+modulo = np.array(lista_real)**2 + np.array(lista_imaginaria)**2
+
+plt.plot(frecuencias, modulo)
+plt.show()
 
 
 #Frecuencias principales
@@ -50,12 +73,17 @@ plt.savefig('RojasLaura_signal.pdf')
 ci1 = incompletos[:,0]
 ci2 = incompletos[:,1]
 
+xmin = min(ci1)
+xmax = max(ci1)
+
+x = np.linspace(xmin,xmax,512)
+
 def interpol_cuadratica(ci1,ci2):
       cuadratica = interp1d(ci1, ci2, kind='quadratic')
       return cuadratica
 
 cuadratica = interpol_cuadratica(ci1,ci2)
-
+y = cuadratica(x)
 
 
 
@@ -64,7 +92,7 @@ def interpol_cubica(ci1,ci2):
     return cubica
 
 cubica = interpol_cubica(ci1,ci2)
-
+y1 = cubica(x)
 
 #Grafica de las 3 transformadas (2 de signal y  de incompletos)
 
