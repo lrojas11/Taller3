0a28df37836d53db3b9fed4112ded0e624fafbe0
Laura Rojas <42391116+lrojas11@users.noreply.github.com>
2018-10-29 21:23:07 -0500

    Add files via upload
    Primera parte

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,3 +1,25 @@
 #Fourier2D.py
 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from scipy import fftpack
+
+#Parte real e imaginaria
+
+transformada = fftpack.fft2(ndimage.imread('arbol.PNG'))
+
+#Calcular modulo para graficar
+
+transformada_modulo = transformada.real**2 + transformada.imag**2
+transformada_modulo = transformada_modulo**0.5
+transformada_modulo = np.log(transformada_modulo)
+
+#Grafica de la transformada
+
+fig = plt.figure()
+plt.imshow(transformada_modulo, cmap=plt.cm.plasma)
+plt.axis('off')
+plt.savefig('RojasLaura_FT2D.pdf')
+
+#Filtro - reducir las magnitudes dentro de 4 elipses horizontales ubicadas en los picos de interes
